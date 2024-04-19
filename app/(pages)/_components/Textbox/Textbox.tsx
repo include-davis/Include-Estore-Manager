b63e3768ce16760db9d58c35b8c93532024cb0da
Amy Vu <amy.anvu@gmail.com>
2024-04-18 17:30:42 -0700

    Added frontend edit/add product form

---
+++ app/(pages)/_components/Textbox/Textbox.tsx
@@ -0,0 +1,96 @@
+import styles from './Textbox.module.scss';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Textbox(props) {
+  const [wordStats, setStats] = useState({
+    words: 0,
+    capacity: false,
+  });
+  function onChange(e) {
+    const temp = e.target.value;
+    const tmp = temp.split(/([\S])+/) || [];
+    const newStats = {
+      words: (tmp.length - 1) / 2,
+      capacity: (tmp.length - 1) / 2 > 300 ? true : false,
+    };
+    setStats(newStats);
+  }
+  return (
+    <div className={styles.textbox_container}>
+      <div className={styles.edit_header}>
+        <div className={styles.icon_group}>
+          <Image src="/icons/bold.svg" alt="bold icon" width={16} height={16} />
+          <Image
+            src="/icons/italicize.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+          <Image
+            src="/icons/underline.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+          <Image
+            src="/icons/strikethrough.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+        </div>
+        <div className={styles.header_border}></div>
+        <div className={styles.icon_group}>
+          <Image
+            src="/icons/columnleft.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+          <Image
+            src="/icons/columnmiddle.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+          <Image
+            src="/icons/columnright.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+        </div>
+        <div className={styles.header_border}></div>
+        <div className={styles.icon_group}>
+          <Image
+            src="/icons/numbers.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+          <Image
+            src="/icons/bullets.svg"
+            alt="bold icon"
+            width={16}
+            height={16}
+          />
+        </div>
+        <div className={styles.header_border}></div>
+      </div>
+      <textarea
+        onChange={onChange}
+        className={styles.input_text}
+        name={props.name}
+        placeholder={props.placeholder}
+      ></textarea>
+      <p
+        className={
+          wordStats.capacity ? styles.wordcount_over : styles.wordcount
+        }
+      >
+        {wordStats.words}/300
+      </p>
+    </div>
+  );
+}
