b320df5b6a631dc52cd1630fd1988e1b951484fa
clarayin129 <clarayin129@gmail.com>
2025-02-10 12:28:28 -0800

    Add Order typedefs, update Product typedef

---
+++ app/(api)/_datalib/_typeDefs/Order.ts
@@ -0,0 +1,71 @@
+import gql from 'graphql-tag';
+
+const typeDefs = gql`
+  type Order {
+    id: ID!
+    products: [Product]
+    customer_name: String!
+    customer_email: String!
+    customer_phone_num: String!
+    billing_address_line_1: String!
+    billing_address_line_2: String
+    billing_city: String!
+    billing_zip: String!
+    billing_country: String!
+    shipping_address_line_1: String!
+    shipping_address_line_2: String
+    shipping_city: String!
+    shipping_zip: String!
+    shipping_country: String!
+    status: String!
+    created_at: DateTime!
+  }
+
+  input OrderInput {
+    customer_name: String!
+    customer_email: String!
+    customer_phone_num: String!
+    billing_address_line_1: String!
+    billing_address_line_2: String
+    billing_city: String!
+    billing_zip: String!
+    billing_country: String!
+    shipping_address_line_1: String!
+    shipping_address_line_2: String
+    shipping_city: String!
+    shipping_zip: String!
+    shipping_country: String!
+  }
+
+  input OrderUpdateInput {
+    customer_name: String
+    customer_email: String
+    customer_phone_num: String
+    billing_address_line_1: String
+    billing_address_line_2: String
+    billing_city: String
+    billing_zip: String
+    billing_country: String
+    shipping_address_line_1: String
+    shipping_address_line_2: String
+    shipping_city: String
+    shipping_zip: String
+    shipping_country: String
+  }
+
+  type Query {
+    order(id: ID!): Order
+    orders(id: [ID]): [Order]
+  }
+
+  type Mutation {
+    createOrder(input: OrderInput!): Order
+    updateOrder(id: ID!, input: OrderUpdateInput!): Order
+    deleteOrder(id: ID!): Boolean
+    addProduct(orderId: ID!, productId: ID!): Order
+    removeProduct(orderId: ID!, productId: ID!): Order
+    editProductQuantity(orderId: ID!, productId: ID!): Order
+  }
+`;
+
+export default typeDefs;
