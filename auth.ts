9ce4497e9f07aaf8f1b2a2dc851af3c9b6fdb754
William Orlopp <beanbeanjuice@outlook.com>
2025-02-10 07:19:55 -0800

    Added Credentials Provider

--- auth.ts
+++ auth.ts
@@ -1,8 +1,162 @@
-import NextAuth from 'next-auth';
+import NextAuth, { CredentialsSignin } from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { prisma } from './prisma';
+import prisma from '@datalib/_prisma/client';
+import Credentials from '@auth/core/providers/credentials';
+import bcrypt from 'bcrypt';
+
+interface Credentials {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  password: string;
+  phone: string;
+  addressLine1?: string;
+  addressLine2?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  zip?: number;
+}
+
+class InvalidLoginError extends CredentialsSignin {
+  constructor(message: string) {
+    super(message);
+    this.code = message;
+  }
+}
+
+async function credentialsSignUp(credentials: Credentials) {
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    phone,
+    addressLine1,
+    addressLine2, // Not required for sign up.
+    city,
+    state, // Not required for sign up.
+    country,
+    zip,
+  } = credentials;
+
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !password ||
+    !phone ||
+    !addressLine1 ||
+    !city ||
+    !country ||
+    !zip
+  ) {
+    throw new InvalidLoginError('Missing fields for sign up');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  return prisma.user.create({
+    data: {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      password: hashedPassword,
+      phone: phone,
+      shipping_address_line_1: addressLine1,
+      shipping_address_line_2: addressLine2,
+      shipping_city: city,
+      shipping_state: state,
+      shipping_country: country,
+      shipping_zip: parseInt(zip),
+    },
+  });
+}
+
+async function credentialsLogIn(credentials: Credentials) {
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    phone,
+    addressLine1,
+    addressLine2,
+    city,
+    state,
+    country,
+    zip,
+  } = credentials;
+
+  // This may be redundant.
+  if (
+    firstName ||
+    lastName ||
+    phone ||
+    addressLine1 ||
+    addressLine2 ||
+    city ||
+    state ||
+    country ||
+    zip
+  ) {
+    throw new InvalidLoginError('Too many fields. Did you mean to sign up?');
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (!user) {
+    throw new InvalidLoginError('User not found');
+  }
+
+  const passwordsMatch = await bcrypt.compare(password, user.password);
+  if (!passwordsMatch) {
+    throw new InvalidLoginError('Invalid password');
+  }
+
+  return user;
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    Credentials({  // http://localhost:3000/api/auth/signin
+      credentials: {
+        firstName: { label: 'First Name', type: 'text' },
+        lastName: { label: 'Last Name', type: 'text' },
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+        phone: { label: 'Phone', type: 'text' },
+        addressLine1: { label: 'Address Line 1', type: 'text' },
+        addressLine2: {
+          label: 'Address Line 2',
+          type: 'text',
+          required: false,
+        },
+        city: { label: 'City', type: 'text' },
+        state: { label: 'State', type: 'text' },
+        country: { label: 'Country', type: 'text' },
+        zip: { label: 'ZIP Code', type: 'number' },
+      },
+      async authorize(credentials: Credentials) {
+        const { email, password } = credentials;
+
+        if (!email || !password) {
+          throw new InvalidLoginError('Email and password are required');
+        }
+
+        const totalUsers = await prisma.user.count();
+
+        if (totalUsers === 0) {
+          // Sign up logic.
+          return credentialsSignUp(credentials); // TODO: Return signed up user.
+        }
+
+        // Login logic.
+        return credentialsLogIn(credentials); // TODO: Return user.
+      },
+    }),
+  ],
 });
