e06fd416e1e72b41de60462ca1c3f8b571dc49f1
William Orlopp <beanbeanjuice@outlook.com>
2025-02-13 20:49:49 -0800

    Converted Sign Up Page to Client Component

--- app/(pages)/signup/page.tsx
+++ app/(pages)/signup/page.tsx
@@ -1,15 +1,27 @@
-'use server';
+'use client';
 
+import { useEffect, useState } from 'react';
 import { signupAllowed } from '@actions/Authorization';
-
 import Signup from './Signup';
-import { redirect, RedirectType } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
-const SignUpWrapper = async () => {
-  const isAllowed = await signupAllowed();
+const SignUpWrapper = () => {
+  const [isAllowed, setIsAllowed] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
 
-  if (!isAllowed) redirect('/login', RedirectType.push);
+  useEffect(() => {
+    const checkSignupAllowed = async () => {
+      const allowed = await signupAllowed();
+      setIsAllowed(allowed);
+      setIsLoading(false);
+    };
 
+    checkSignupAllowed();
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>; // TODO: Probably make this look nicer.
+  if (!isAllowed) router.push('/login');
   return <Signup />;
 };
 
