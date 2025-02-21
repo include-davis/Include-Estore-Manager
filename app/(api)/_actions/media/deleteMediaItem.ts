b57d7bebe0cda34b7f90fa0d0c903e24f956ae5d
VinBid <115661286+VinBid@users.noreply.github.com>
2025-02-20 19:35:39 -0800

    * Updated and fixed import errors * Not confident in my typescript types.

--- app/(api)/_actions/media/deleteMediaItem.ts
+++ app/(api)/_actions/media/deleteMediaItem.ts
@@ -1,7 +1,8 @@
 'use server';
 import { deleteMediaItem } from '@datalib/media/deleteMediaItem';
 import { revalidatePath } from 'next/cache';
-import WithCallback from '@app/(api)/_utils/callback/withCallback';
+// import WithCallback from '@app/(api)/_utils/callback/withCallback';
+import WithCallback from '@utils/callback/withCallback';
 
 export const DeleteMediaItem = WithCallback(async (id: string) => {
   const deleteMediaRes = await deleteMediaItem(id);
