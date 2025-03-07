542855c69e39924457fc960a2e71060d723507f5
beanbeanjuice <beanbeanjuice@outlook.com>
2025-03-06 19:47:20 -0800

    Updated Apollo Context

--- app/(api)/_datalib/apolloServer.ts
+++ app/(api)/_datalib/apolloServer.ts
@@ -1,26 +1,25 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import type { NextRequest } from 'next/server';
 
 import typeDefs from './_typeDefs/index';
 import resolvers from './_resolvers/index';
 import { auth } from '@/auth';
 import { Session } from 'next-auth';
 
-const server = new ApolloServer({
+const server = new ApolloServer<ApolloContext>({
   typeDefs,
   resolvers,
   introspection: process.env.NODE_ENV !== 'production',
-}) as ApolloServer<object>;
+});
 
 /**
  * Can add extra things here when necessary.
  */
-interface ApolloContext {
+export interface ApolloContext {
   session: Session | null;
 }
 
-const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+const handler = startServerAndCreateNextHandler(server, {
   context: async (): Promise<ApolloContext> => {
     const session = await auth();
     return { session };
