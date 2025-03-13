571ef94d383254ad00ef27311346572dabce9b44
VinBid <115661286+VinBid@users.noreply.github.com>
2025-03-13 16:47:46 -0700

    updated schema and fixed crud to fix format

--- app/(api)/_utils/callback/withCallback.ts
+++ app/(api)/_utils/callback/withCallback.ts
@@ -1,10 +1,10 @@
-import callbacks from '@app/_utils/callbacks';
+// import callbacks from '@app/_utils/callbacks';
 
-type CallbackFunction = (...args: any[]) => any;
-export default function WithCallback(func: (..._: any) => any) {
-  return async (...args: any) => {
-    const res = await func(...args);
-    callbacks.onUpdate();
-    return res;
-  };
-}
+// type CallbackFunction = (...args: any[]) => any;
+// export default function WithCallback(func: (..._: any) => any) {
+//   return async (...args: any) => {
+//     const res = await func(...args);
+//     callbacks.onUpdate();
+//     return res;
+//   };
+// }
--- app/(api)/_utils/request/getQueries.ts
+++ app/(api)/_utils/request/getQueries.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from 'next/server';
 import prisma from '@datalib/_prisma/client';
-// import { DMMF } from '@prisma/client/runtime';
+import { DMMF } from '@prisma/client/runtime';
 
 function typeCast(value: string, type: string) {
   switch (type) {
@@ -36,16 +36,18 @@ export default async function getQueries(
     fields: Field[];
   }
 
-  const schema: Schema | undefined = prisma._getDmmf().datamodel.models.find(
-    (model: { name: string }) => model.name === content_type
-  );
+  const schema: Schema | undefined = prisma
+    ._getDmmf()
+    .datamodel.models.find(
+      (model: { name: string }) => model.name === content_type
+    );
 
   if (!schema) {
     throw new Error(`Model ${content_type} not found in Prisma schema.`);
   }
 
   const query_entries = request.nextUrl.searchParams.entries();
-  const output: { [key: string]: any } = {};
+  const output: { [key: string]: string | number | boolean } = {};
 
   for (const [key, val] of query_entries) {
     // Fetch the field type from the Prisma schema
