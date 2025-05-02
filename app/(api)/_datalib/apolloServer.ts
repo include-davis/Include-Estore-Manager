95b1e1eea10dc76f787a07a8839b35ab8dd9aea0
William Orlopp <beanbeanjuice@outlook.com>
2025-05-01 18:26:33 -0700

    Add API Protection
    This commit adds API protection via an API key. This fixes issue #68 on
    GitHub.

--- app/(api)/_datalib/apolloServer.ts
+++ app/(api)/_datalib/apolloServer.ts
@@ -6,6 +6,11 @@ import { auth } from '@/auth';
 import prisma from '@datalib/_prisma/client';
 import typeDefs from './_typeDefs/index';
 import resolvers from './_resolvers/index';
+import { NextRequest } from 'next/server';
+
+const ALLOWED_ORIGINS = (process.env.ALLOWED_ORIGINS || '')
+  .split(',')
+  .map((s) => s.trim());
 
 const server = new ApolloServer<ApolloContext>({
   typeDefs,
@@ -19,20 +24,48 @@ const server = new ApolloServer<ApolloContext>({
 export interface ApolloContext {
   session: Session | null;
   isOwner: boolean;
+  hasValidApiKey: boolean;
+}
+
+async function apiKeyIsValid(req: NextRequest) {
+  const authHeader = req.headers.get('Authorization') || '';
+  const apiKey = authHeader.replace('Bearer ', '');
+  const origin = req.headers.get('Origin') || '';
+  const referer = req.headers.get('Referer') || '';
+  const isLocalDevelopment = process.env.NODE_ENV === 'development';
+
+  const hasValidApiKey = apiKey === process.env.API_KEY;
+  const hasValidOrigin =
+    ALLOWED_ORIGINS.includes(origin) ||
+    ALLOWED_ORIGINS.some((domain) => referer.startsWith(domain)); // I believe this can be spoofed. Good to use in combination with API key.
+
+  // If an API key is provided, ensure it's valid and comes from an allowed origin
+  return hasValidApiKey && (hasValidOrigin || isLocalDevelopment);
 }
 
 const handler = startServerAndCreateNextHandler(server, {
-  context: async (): Promise<ApolloContext> => {
-    const session = await auth();
+  context: async (req: NextRequest): Promise<ApolloContext> => {
+    // If there's no valid API key, fallback to session auth
+    /*
+    TODO: There are different ways to do this. We can make it so that if the API key is invalid,
+    instead of falling back to session auth we completely ignore this attempt to connect to the server at all
+    (by throwing an error). This ensures that if anyone attempts to use an API key, session auth will not be used.
+     */
+    const hasValidApiKey = await apiKeyIsValid(req);
+    let session: Session | null = null;
+    let isOwner = false;
 
-    const uid = session?.user?.id;
-    const dbUser = await prisma.user.findUnique({ where: { id: uid } });
-
-    const isOwner = uid != undefined && dbUser !== null;
+    if (!hasValidApiKey) {
+      session = await auth();
+      const uid = session?.user?.id;
+      const dbUser = await prisma.user.findUnique({ where: { id: uid } });
+      isOwner = uid != undefined && dbUser !== null;
+    }
 
     return {
       session,
       isOwner,
+      hasValidApiKey,
     };
   },
 });
