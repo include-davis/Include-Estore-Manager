aa17f67218785d16552efc8c48364a867583b6c9
brandonw504 <brandonw504@outlook.com>
2025-07-10 17:27:31 -0700

    Switched to require

--- scripts/run-migrations.js
+++ scripts/run-migrations.js
@@ -1,4 +1,5 @@
-import { exec } from 'child_process';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const exec = require('child_process');
 
 const MIGRATION_COMMAND = 'npx prisma migrate deploy';
 const MAX_RETRIES = 5;
